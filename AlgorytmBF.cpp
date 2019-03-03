a44410e91cdc5bc48c1df4b95399cd4e80412128
Michal Danaj <michal.danaj@gmail.com>
2019-03-03 21:13:55 +0100

    Wyklepany kod algorytmu BF

--- AlgorytmBF.cpp
+++ AlgorytmBF.cpp
@@ -6,19 +6,21 @@ AlgorytmBF::AlgorytmBF(Plecaki *plecaki_, const list<Przedmiot>& przedm):
 {
     plecaki = plecaki_;
     
-    for (auto &el:przedm)
+    w_ktorym = vector<int>(ile_prz); //w którym plecaku znajduje siê przedmiot
+                                     //przy starcie w zerowym
+                                     
+    ile_przed = vector<int>(ile_plec); //ile przedmiotow jest w plecaku
+    ile_przed[0]=ile_prz;  //wszystko w puli
+
+    for (auto &el:przedm){
         przedmioty.push_back(el);
+    }
     
     //tworzê tabelê z wartoœciami
     wartosci = new double*[ile_prz];
     for (int i=0; i < ile_prz; i++)
         wartosci[i] = new double[ile_plec];
-        
-    //tworzê tabelê czy odwiedzony
-    odwiedzony = new int*[ile_prz];
-    for (int i=0; i < ile_prz; i++)
-        odwiedzony[i] = new int[ile_plec];
-        
+                
     //Wype³niam wartoœciami, poza pul¹
     for (int i=0; i < ile_prz; i++)
         for (int j=1; j < ile_plec; j++)
@@ -27,13 +29,6 @@ AlgorytmBF::AlgorytmBF(Plecaki *plecaki_, const list<Przedmiot>& przedm):
     for (int i=0; i < ile_prz; i++)
         wartosci[i][0] = 0;
         
-    //Wype³niam wartoœciami, poza pul¹
-    for (int i=0; i < ile_prz; i++)
-        for (int j=1; j < ile_plec; j++)
-            odwiedzony[i][j] = 0;            
-    //Wype³niam wartoœciami w puli
-    for (int i=0; i < ile_prz; i++)
-        odwiedzony[i][0] = 1;
     
 }
 
@@ -42,22 +37,52 @@ AlgorytmBF::~AlgorytmBF(){
     for (int i=0; i < ile_prz; i++)
         delete wartosci[i];
     delete wartosci;
-
-    for (int i=0; i < ile_prz; i++)
-        delete odwiedzony[i];
-    delete odwiedzony;
-    
+   
+}
+void AlgorytmBF::przeloz(int i, int plecak_akt, int plecak_doc){
+    w_ktorym[i] = plecak_doc;
+    ile_przed[plecak_akt]--;
+    ile_przed[plecak_doc]++;
+    ocena_akt = -wartosci[i][plecak_akt]+wartosci[i][plecak_doc];
+    if (ocena_akt > ocena_max){
+        ocena_max = ocena_akt;
+        kombinacja_max = w_ktorym;
+    }
 }
 
+void AlgorytmBF::licz(){
+    
+    //dla ka¿dego plecaka
+    //umieszczamy w nim i-ty przedmiot i wywo³ujemy przeszukiwanie
+    //dla kolejnego kroku
+    for (int j=1; j < ile_plec; j++)
+        if (ile_przed[j] < plecaki->poj(j))
+            for (int i=0; i < ile_prz; i++) {
+                //jeœli jest w puli
+                if (w_ktorym[i] == 0){
+                    //wk³ada do plecaka
+                    przeloz(i, 0, j);                      
+                    //wywo³uje dalsze przeszukiwanie
+                    licz();
+                    //przek³ada do puli
+                    przeloz(i, j, 0);
+                }
+            }                
+}
 
 //TODO: na razie algorytm zak³ada, ¿e wszystkie przedmioty s¹ w puli na sart
 void AlgorytmBF::start(){
+
+    ocena_akt = 0;
+    ocena_max = 0;
     
     //czy oby na pewno wszystkie przedmioty s¹ w puli
     for (int i=1; i < ile_plec; i++)
         if (plecaki->size(i) > 0)
             throw string("Przy starcie algorytmu plecaki musza byc puste!");
 
-};
+    licz();
+}
+
 
 
--- AlgorytmBF.h
+++ AlgorytmBF.h
@@ -7,15 +7,23 @@
 
 class AlgorytmBF
 {
-    double** wartosci; //tablica z wartoœciami przedmiotow w plecaku
-    int** odwiedzony;  //tablica z informacj¹, który przedmiot 
-                                        //w którym plecaku
-    Plecaki* plecaki;   //wskaŸnik do plecaków
-    
     const int ile_plec;
     const int ile_prz;
+
+    double** wartosci; //tablica z wartoœciami przedmiotow w plecaku
+    Plecaki* plecaki;   //wskaŸnik do plecaków
+    
+    vector<int> w_ktorym; //w którym plecaku znajduje siê przedmiot
+    vector<int> ile_przed; //ile przedmiotow jes w plecaku
     
     vector<Przedmiot> przedmioty;
+    
+    vector<int> kombinacja_max;   //kombinacja przedmiotów maksymalizuj¹ca wynik
+    double ocena_akt=0; //aktualna wartoœæ przedmiotów w plecakach
+    double ocena_max=0; //maksymalna wartoœæ przedmiotów w plecakach    
+    
+    void licz();
+    void przeloz(int i, int plecak_akt, int plecak_doc);
     
   public:
       
@@ -25,6 +33,7 @@ class AlgorytmBF
     
     //TODO: na razie algorytm zak³ada, ¿e wszystkie przedmioty s¹ w puli na sart
     void start();
+    
 };
 
 #endif
